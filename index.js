b332718fe82ffbfc88719e6d2ce32e6bc246fee3
viridiana hernandez <vbhg20@gmail.com>
2019-05-16 09:56:51 -0700

    primeros tres metodos

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const path = require("path");
 app.all('/hello', function (req, res) {
   res.send('Hello World!');
 });
-
 app.all('/bye', function (req, res) {
   res.send('Bye bye :)');
 });
-
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
-
-
+app.get('/algo1', function (req, res) {
+  res.status(202)send('algo 1');
+});
+app.post('/algo2', function (req, res) {
+  res.status(200).send({algo2: perrito});
+});
+app.patch('/algo3', function (req, res) {
+  res.status(401)send('esto se actualizo');
+});
 app.listen(3000, function () {
   console.log('My app listening on port 3000!');
 });
