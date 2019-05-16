51bfbbed75a32ca53322ee0111ba34b2390fd8ad
viridiana hernandez <vbhg20@gmail.com>
2019-05-16 10:00:07 -0700

    correcciones

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.all('/bye', function (req, res) {
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
 app.get('/algo1', function (req, res) {
-  res.status(202)send('algo 1');
+  res.status(202).send('algo 1');
 });
 app.post('/algo2', function (req, res) {
-  res.status(200).send({algo2: perrito});
+  res.status(200).send({algo2: "perrito"});
 });
 app.patch('/algo3', function (req, res) {
-  res.status(401)send('esto se actualizo');
+  res.status(401).send('esto se actualizo');
 });
 app.listen(3000, function () {
   console.log('My app listening on port 3000!');
